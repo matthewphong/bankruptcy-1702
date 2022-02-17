5b54dd76190ec056f3ae7e8b6d6389c12c2ea222
matthewphong <99379552+matthewphong@users.noreply.github.com>
2022-02-17 14:14:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ from keras.models import load_model
 @app.route("/", methods =["GET","POST"])
 def index():
     if request.method == "POST":
-        #NPTA= request.form.get("NPTA")
-        #TLTA= request.form.get("TLTA")
-        #WCTA= request.form.get("WCTA")
-        #print(NPTA,TLTA,WCTA)
-        #model=load_model("bankruptcy")
-        #pred= model.predict([[float(NPTA),float(TLTA),float(WCTA)]])
-        #s = "The predicted bankruptcy score is: " + str(pred)
-        return(render_template("index.html", result="1"))
+        NPTA= request.form.get("NPTA")
+        TLTA= request.form.get("TLTA")
+        WCTA= request.form.get("WCTA")
+        print(NPTA,TLTA,WCTA)
+        model=load_model("bankruptcy")
+        pred= model.predict([[float(NPTA),float(TLTA),float(WCTA)]])
+        s = "The predicted bankruptcy score is: " + str(pred)
+        return(render_template("index.html", result=s))
     else:
         return(render_template("index.html", result="2"))
 
